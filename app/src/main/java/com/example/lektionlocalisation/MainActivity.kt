d18c26a74d9faf642f211d0a3c789c9e2333c5d3
Olof Hammar <olle_hammat@hotmail.com>
2020-09-16 10:46:48 +0200

    Added translate main activity layout to swedish

--- app/src/main/java/com/example/lektionlocalisation/MainActivity.kt
+++ app/src/main/java/com/example/lektionlocalisation/MainActivity.kt
@@ -2,11 +2,26 @@ package com.example.lektionlocalisation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+import android.util.Log
+import android.view.View
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
+lateinit var greeting: TextView
+var name: String = "Olle"
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        greeting = findViewById(R.id.tv_greeting)
+
+    }
+
+    fun btnPressed (v: View) {
+        //För att lägga in en placeholder i strängen används %1$s för string (1 betyder att det är första placeholdern och
+        //skrivs %2$s om det ska läggas till en till placeholder i samma sträng. Här nedan skickar vi med name som
+        //placeholder.
+        greeting.text = getString(R.string.great, name)
     }
 }
 
